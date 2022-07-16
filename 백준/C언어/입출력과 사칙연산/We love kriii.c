c4dce1c973e3c80176a3dcd1a2312792f839de42
seoyoung0714 <100742523+seoyoung0714@users.noreply.github.com>
2022-07-16 21:46:07 +0900

    Rename 백준/C언어/We love kriii.c to 백준/C언어/입출력과 사칙연산/We love kriii.c

---
+++ 백준/C언어/입출력과 사칙연산/We love kriii.c
@@ -0,0 +1,14 @@
+// 10718
+// We love kriii
+// C언어
+// 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력하시오.
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("강한친구 대한육군\n");
+	printf("강한친구 대한육군\n");
+
+	return 0;
+}
