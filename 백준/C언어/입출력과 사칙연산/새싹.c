f0a1ac85d5a106e9865c959ca091a684b2dc5d9b
seoyoung0714 <100742523+seoyoung0714@users.noreply.github.com>
2022-07-16 21:51:21 +0900

    Update 새싹.c

--- 백준/C언어/입출력과 사칙연산/새싹.c
+++ 백준/C언어/입출력과 사칙연산/새싹.c
@@ -9,7 +9,7 @@ int main(void)
 {
     printf("         ,r\'\"7\n");
     printf("r`-_   ,\'  ,/\n");
-    printf(" \. \". L_r\'\n");
+    printf(" \\. \". L_r\'\n");
     printf("   `~\\/\n");
     printf("      |\n");
     printf("      |\n");
